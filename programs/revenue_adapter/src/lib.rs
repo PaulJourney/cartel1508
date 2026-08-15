b837f2c8affb9680090431813b22e99c18d13549
Paul Journey <76806868+PaulJourney@users.noreply.github.com>
2026-08-15 10:26:11 +0200

    fix Anchor 1.1 CPI program id

--- programs/revenue_adapter/src/lib.rs
+++ programs/revenue_adapter/src/lib.rs
@@ -144,7 +144,7 @@ pub mod revenue_adapter {
         };
 
         let cpi_ctx = CpiContext::new_with_signer(
-            ctx.accounts.referral_program.to_account_info(),
+            ctx.accounts.referral_program.key(),
             cpi_accounts,
             signer_seeds,
         );
